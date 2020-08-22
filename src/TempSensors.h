6d55547366f1f8f96939a276ec5a89e6cd459b17
Przemek Talarek <przemyslaw.talarek@kinguin.io>
2020-08-22 21:40:13 +0200

    getCount for temperature sensors

--- src/TempSensors.h
+++ src/TempSensors.h
@@ -3,6 +3,7 @@
 #include <ArduinoJson.h>
 
 #define TEMP_REQUEST_INTERVAL 10000
+#define FORCE_SEND_UPDATE_INTERVAL 60000
 #define TEMPERATURE_PRECISION 9
 
 typedef std::function<void(const char*, float)> TemperatureChangedCallback;
@@ -17,10 +18,14 @@ class TempSensors : DallasTemperature {
         }
         std::vector<String> getNames();
         std::vector<float> getStates();
+        uint8_t getCount() {
+            return _ds.getDS18Count();
+        }
     private:
         DallasTemperature _ds;
         OneWire _oneWire;
         TemperatureChangedCallback _valuesChangedCallback;
         unsigned long _lastTempRequestTime = 0;
+        unsigned long _lastForceSendTime = 0;
         std::vector<float> _lastValues = {};
 };
